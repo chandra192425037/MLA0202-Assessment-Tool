4d7c157bd804556c538b36b71945257b7c3f2c1e
chandra192425037 <gcreddy8125@gmail.com>
2026-07-21 09:56:31 +0530

    Update code.py

--- C01-AT1/QUESTION2/code.py
+++ C01-AT1/QUESTION2/code.py
@@ -1,1 +1,15 @@
-
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score
+data = pd.read_csv("E:/New folder/New folder/Student_performance.csv")
+X = data.iloc[:, :-1]
+y = data.iloc[:, -1]
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+model = LinearRegression()
+model.fit(X_train, y_train)
+y_pred = model.predict(X_test)
+print("Predicted Marks:", y_pred)
+print("R2 Score:", r2_score(y_test, y_pred))
