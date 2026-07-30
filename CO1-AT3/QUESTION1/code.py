3f53be74774de7db5d26aaaca11d227049cce85a
chandra192425037 <gcreddy8125@gmail.com>
2026-07-30 08:31:11 +0530

    Update code.py

--- CO1-AT3/QUESTION1/code.py
+++ CO1-AT3/QUESTION1/code.py
@@ -1,1 +1,17 @@
-
+import pandas as pd
+from sklearn.datasets import load_breast_cancer
+data = load_breast_cancer()
+X = pd.DataFrame(data.data, columns=data.feature_names)
+y = pd.Series(data.target)
+print("Classes:", data.target_names)
+class_counts = y.value_counts().sort_index()
+total = len(y)
+prob_malignant = class_counts[0] / total
+prob_benign = class_counts[1] / total
+print("\nProbability of Malignant:", prob_malignant)
+print("Probability of Benign:", prob_benign)
+if prob_benign > prob_malignant:
+    prediction = "Benign"
+else:
+    prediction = "Malignant"
+print("\nPredicted class for a new instance:", prediction)
