9ea021808fee5d5d03d1d3971448eefa5f9f6f63
chandra192425037 <gcreddy8125@gmail.com>
2026-07-30 08:43:07 +0530

    Update code.py

--- CO1-AT3/QUESTION2/code.py
+++ CO1-AT3/QUESTION2/code.py
@@ -1,1 +1,34 @@
-
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+data = {
+    "label": [
+        "ham", "ham", "ham", "ham", "ham",
+        "spam", "spam", "spam", "spam", "spam"
+    ],
+    "message": [
+        "How are you?",
+        "Let's meet tomorrow.",
+        "Are you coming to class?",
+        "Call me when you reach home.",
+        "Can we have lunch today?",
+        "Congratulations! You won a free lottery.",
+        "Claim your cash prize now.",
+        "Win a free mobile phone.",
+        "Exclusive offer! Click here.",
+        "You have won a $1000 gift card."
+    ]
+}
+df = pd.DataFrame(data)
+X = df["message"]
+y = df["label"]
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(X)
+model = MultinomialNB()
+model.fit(X, y)
+test_message = ["Congratulations! You won a free ticket."]
+test = vectorizer.transform(test_message)
+print("Posterior Probabilities:")
+print(model.predict_proba(test))
+prediction = model.predict(test)
+print("Prediction:", prediction[0])
